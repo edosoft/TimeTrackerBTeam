2179c0981d5bd926b17f74fc09f3be3b21be0741
Nestor Marin <nestor.marin@edosoft.es>
2017-11-22 12:33:43 +0000

    Added automated logic to checkin and checkout

--- Server/main.py
+++ Server/main.py
@@ -85,9 +85,25 @@ class MainPage(remote.Service):
 
         # querycheckin has the Workday of the employee in the proper day.
         if querycheckin.checkin is None:
-            querycheckin.checkin = datetime.datetime.now()
-            querycheckin.put()
-            return CheckinResponseMessage(response_code=200, text="Initializing Check in", checkin = str(querycheckin.checkin))
+            now = datetime.datetime.now()
+            checkmin = now.replace(hour=7, minute=30, second=59, microsecond=0)
+            checkmax = now.replace(hour=9, minute=00, second=59, microsecond=0)
+
+            if now < checkmin:
+                return CheckinResponseMessage(response_code=400,
+                                              text="You can't check in before 7:30 am")
+            else:
+                querycheckin.checkin = now
+                querycheckin.put()
+
+                if now < checkmax:
+                    return CheckinResponseMessage(response_code=200,
+                                                  text="Successful Check in",
+                                                  checkin=str(querycheckin.checkin))
+                else:
+                    return CheckinResponseMessage(response_code=200,
+                                                  text="Check in out of time",
+                                                  checkin=str(querycheckin.checkin))
         else:
             return CheckinResponseMessage(response_code=400, text="You can't check in again today")
 
@@ -100,17 +116,45 @@ class MainPage(remote.Service):
         querycheckout = Workday.query(Workday.employeeid == user.email(),
                                       Workday.date == datetime.datetime.now()).get()
 
+        if not querycheckout.checkout is None: 
+            return CheckoutResponseMessage(response_code=400,
+                                           text="You can't check out if you checked out already")
+
         # Querywork has the Workday of the employee in the proper day.
         if querycheckout.checkin is None:
             return CheckoutResponseMessage(response_code=400,
                                            text="You can't check out without checking in")
         else:
-            querycheckout.checkout = datetime.datetime.now()
-            querycheckout.total = (querycheckout.checkout - querycheckout.checkin).seconds / 3600
-            querycheckout.put()
-            return CheckoutResponseMessage(response_code=200, \
-        text="Checkout Ok. Have a nice day :)", checkout=str(querycheckout.checkout), \
-        total=querycheckout.total)
+            now = datetime.datetime.now()
+            checkmin = now.replace(hour=13, minute=00, second=0, microsecond=0)
+            checkmax = now.replace(hour=19, minute=00, second=0, microsecond=0)
+            checknoon = now.replace(hour=15, minute=00, second=0, microsecond=0)
 
+            querycheckout.checkout = datetime.datetime.now()
+            querycheckout.total = (querycheckout.checkout - querycheckout.checkin).seconds / 60
+
+            if now < checkmin:
+                querycheckout.put()
+                return CheckoutResponseMessage(response_code=400,
+                                               text="You checked out too early",
+                                               checkout=str(querycheckout.checkout),
+                                               total=querycheckout.total)
+            else:
+                if now > checknoon: #If you go out after 15:00, a hour is substracted from the total
+                    querycheckout.total = querycheckout.total -60              
+
+                if now < checkmax:
+                    querycheckout.put()
+                    return CheckoutResponseMessage(response_code=200, \
+                                                   text="Checkout Ok. Have a nice day :)",
+                                                   checkout=str(querycheckout.checkout), \
+                                                   total=querycheckout.total)
+                else:
+                    querycheckout.checkout = checkmax
+                    querycheckout.put()
+                    return CheckoutResponseMessage(response_code=200,
+                                                   text="Check out out of time",
+                                                   checkout=str(querycheckout.checkout), \
+                                                   total=querycheckout.total)
 
 app = endpoints.api_server([MainPage], restricted=False)
