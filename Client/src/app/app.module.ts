5e0274b6eeb59930f0b2fe258987decd7ee40269
Deyán <deyan.guacaran@edosoft.es>
2017-12-14 14:32:46 +0000

    Menu responsive done

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServerProvider } from './provider/server.provider';
 
 import { AppRoutingModule } from './provider/router.provider';
@@ -22,7 +23,8 @@ import { CanActivateViaUserWorkdayGuard } from './provider/guard.provider';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [ServerProvider],
   bootstrap: [AppComponent]
