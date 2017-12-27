b877aab81dd970ca5367de93c83f617b6ce2510e
Luis Montero <luis.montero@edosoft.es>
2017-12-27 09:35:07 +0000

    Personal issues working

--- Client/src/app/issues/issues.component.ts
+++ Client/src/app/issues/issues.component.ts
@@ -10,13 +10,31 @@ import { ServerProvider } from '../provider/server.provider';
 export class IssuesComponent implements OnInit {
   issues_per_employee: any = [];
   JSON = JSON;
+  personalView= false;
+  changeViews= false;
+  personalArray: any = [];
 
   constructor(private server: ServerProvider) { }
 
   ngOnInit() {
     this.server.getUserWithIssues().then((response) => this.issues_per_employee = response.issues_per_employee);
   }
-
-
-
+  employeeView(row) {
+    this.personalView = true;
+    this.personalArray = row;
+    console.log(this.personalArray.issues);
+  }
+  changeView(issue) {
+    this.changeViews = true;
+    this.personalView = false;
+    console.log(issue);
+  }
+  swapper() {
+    if (this.changeViews == true) {
+      this.changeViews = false;
+      this.personalView = true;
+    } else {
+      this.personalView = false;
+    }
+  }
 }
