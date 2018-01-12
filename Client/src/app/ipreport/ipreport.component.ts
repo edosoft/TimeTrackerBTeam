873917664afd3cde77e053584200bc568b1aabe3
Maria Ramos <maria.ramos@edosoft.es>
2018-01-12 15:55:04 +0000

    IP report: IP list by date (all users) finished

--- Client/src/app/ipreport/ipreport.component.ts
+++ Client/src/app/ipreport/ipreport.component.ts
@@ -14,6 +14,7 @@ export class IpreportComponent implements OnInit {
   currentDate: string;
   selectedDate: string;
   invalidDate: boolean;
+  noRecordsFound: boolean;
 
   constructor(private server: ServerProvider) { }
 
@@ -31,30 +32,25 @@ export class IpreportComponent implements OnInit {
       this.selectedDate = this.server.getUserWorkday().date;
     } else {
       this.server.getListIPUser(this.selectedDate).then((response) => {
-      this.ipUsers = response.ip_report;
-      });
-    }
-  }
-
-  close() {
-    this.invalidDate = false;
-  }
-
- /* getReport() {
-    
-      const body = {
-        date: this.selectedDate,
-        report_type: this.reportType
-      };
-      this.server.getReport(body).then((response) => {
         if (response.response_code == 400) {
           this.noRecordsFound = true;
           this.selectedDate = this.server.getUserWorkday().date;
         } else {
           this.noRecordsFound = false;
-          this.generateWorkdays(response);
+          this.ipUsers = response.ip_report;
         }
       });
-  }*/
-  
+    }
+  }
+
+  closeInvalidDate() {
+    this.invalidDate = false;
+  }
+
+  closeNoRecordsFound() {
+    this.noRecordsFound = false;
+  }
+
+
+
 }
